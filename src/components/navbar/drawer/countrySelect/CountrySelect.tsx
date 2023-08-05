ca47ee0a620e72703a38acf398ded6350a344789
RafalGontarski <gontarskirafal@gmail.com>
2023-08-05 13:53:28 +0200

    fix_MSH-3/registration_drawer_country_selector_refactor

--- src/components/navbar/drawer/countrySelect/CountrySelect.tsx
+++ src/components/navbar/drawer/countrySelect/CountrySelect.tsx
@@ -5,6 +5,10 @@ import Box from '@mui/material/Box';
 function CountrySelect() {
     const [country, setCountry] = React.useState('Polska');
 
+    const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+        setCountry(event.target.value);
+    };
+
     const getFlagIcon = (countryName: any) => {
         switch (countryName) {
             case 'Polska':
@@ -41,20 +45,67 @@ function CountrySelect() {
             >
             </InputLabel>
             <Select
-                value={country}
-                onChange={(e) => setCountry(e.target.value)}
                 label="Kraj"
+                value={country}
+                onChange={handleChange}
+                fullWidth
+                sx={{
+                    width: '350px',
+                    '&:hover': { backgroundColor: '#fff' },
+                    '& .MuiOutlinedInput-root': {
+                        fontFamily: 'Arial, sans-serif',
+                        '&.Mui-focused fieldset': {
+                            borderColor: '#000000',
+                            borderWidth: '1px',
+                        },
+                        '&:hover fieldset': {
+                            borderColor: '#000000',
+                            borderWidth: '1px',
+                        },
+                    },
+                    '& .MuiInputLabel-root': {
+                        fontSize: '12px',
+                        transform: 'translate(14px, 12px) scale(1)', // Adjust this value to center the label
+                        '&.Mui-focused': {
+                            color: '#000000', // Change this to the color you want
+                            fontSize: '12px',
+                        },
+                    },
+                    '& .MuiInputLabel-outlined.MuiInputLabel-shrink': {
+                        transform: 'translate(18px, -5px) scale(0.7)', // Adjust this value to center the label
+                    },
+                    '& .MuiOutlinedInput-notchedOutline': {
+                        // borderColor: '#000000', // This will change the default border color
+                    },
+                    '&:hover .MuiOutlinedInput-notchedOutline': {
+                        borderColor: '#000000', // This will change the border color on hover
+                        borderWidth: '1px',
+                    },
+                    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                        borderColor: '#000000', // This will change the border color when the Select is focused
+                        borderWidth: '1px',
+                    },
+                    '& .MuiSelect-select': {
+                        fontFamily: 'Arial, sans-serif',
+                        fontSize: '12px',
+                        color: '#000000', // This will change the color of the selected option
+                        '&:hover, &.Mui-focused': {
+                            color: '#000000', // This will change the color on hover and focus
+                        },
+                    },
+                    '& .MuiMenuItem-root': {
+                        fontFamily: 'Arial, sans-serif',
+                        fontSize: '12px',
+                        color: '#808080', // This will change the color of the menu options
+                        '&:hover': {
+                            color: '#000000', // This will change the color on hover
+                        },
+                    },
+                }}
                 renderValue={(selectedValue) => (
-                    <Box
-                        display="flex"
-                        alignItems="center">
+                    <Box display="flex" alignItems="center">
                         {getFlagIcon(selectedValue)}
-                        <Typography
-                            variant="inherit"
-                            sx={{
-                                marginLeft: 1
-                            }}
-                        >
+                        <Typography variant="inherit" sx={{ marginLeft: 1 }}>
                             {selectedValue}
                         </Typography>
                     </Box>
