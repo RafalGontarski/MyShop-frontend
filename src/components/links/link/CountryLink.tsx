051f85d4b96ca6ae15062e833c06e4b82027c2ec
RafalGontarski <gontarskirafal@gmail.com>
2023-08-07 11:33:25 +0200

    fix_MSH-4/country_link

--- src/components/links/link/CountryLink.tsx
+++ src/components/links/link/CountryLink.tsx
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '@mui/material/Link';
+import FlagIcon from '@mui/icons-material/Flag';
+import CheckIcon from '@mui/icons-material/Check';
 
 interface CustomLinkProps {
     label: string;
-    onClick?: () => void; // Oznacza, że onClick jest opcjonalny
+    selected: boolean;
+    onClick?: () => void;
 }
 
-const CountryLink: React.FC<CustomLinkProps> = ({ label, onClick }) => {
+const CountryLink: React.FC<CustomLinkProps> = ({ label, selected, onClick }) => {
     return (
         <Link
             href="#"
             style={{
                 color: '#000',
                 textDecoration: 'none',
-                fontSize: '0.8rem' }}
+                fontSize: '0.8rem',
+                display: 'block'}}
             underline="none"
             onMouseOver={(event) => {event.currentTarget.style.color = '#008000'}}
             onMouseOut={(event) => {event.currentTarget.style.color = '#000'}}
             onClick={onClick}
         >
+            <FlagIcon />
             {label}
+            {selected && <CheckIcon />}
         </Link>
     );
 };
+
 
 export default CountryLink;
 
